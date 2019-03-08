d005f1771bf1a7c829c76a7cf367c2ca485ffbd8
Arunav Gupta <arunav.gupta@nike.com>
2019-03-08 14:54:27 -0800

    fix logic

--- assets/logic/app.js
+++ assets/logic/app.js
@@ -7,10 +7,12 @@ let dataStore = {
 
 $(`#searchBtn`).on("click", function () {
     let searchCriteria = 'community';
-    let url = "https://predicthq.p.rapidapi.com/v1/events/?category=" + searchCriteria + "&offset=10"
+    let url = "https://predicthq.p.rapidapi.com/v1/events/?category=" + searchCriteria + "&offset=10";
+    let method ="GET";
+   
     $.ajax({
         url,
-        method: "GET",
+        method,
         headers: {
             "authorization": "Bearer tbJK965oKbaCIoKx1HpePpSe3h12kH",
             "x-rapidapi-key": "8addb057bamsh9ccd55dc462f4f4p1237d6jsn8ed930a43da9"
